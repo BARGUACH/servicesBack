e1cfbb0991fca1f4e046b2d21ec43d0297350bc6
Lvoix <zakaria.khobizi92@gmail.com>
2023-02-09 01:38:27 +0100

    first commit

---
+++ src/main/java/com/gestions/services/ServicesBackApplication.java
@@ -0,0 +1,13 @@
+package com.gestions.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestions/services/ServicesBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestions.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
